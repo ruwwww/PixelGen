d664448b8e38789297faa12adb3c657845cd2146
Your Name <you@example.com>
2026-02-17 02:57:24 +0000

    Update metrics calculation in compute_torch_fidelity_metrics: include precision and recall in kwargs

--- compute_torch_fidelity_metrics.py
+++ compute_torch_fidelity_metrics.py
@@ -63,7 +63,7 @@ def run():
         print(f"Computing metrics for {exp_name}/{step_name} -> {step_path}")
         try:
             # call calculate_metrics; be permissive about kwargs
-            kwargs = dict(input1=str(real_stats), input2=str(step_path), verbose=False)
+            kwargs = dict(input1=str(real_stats), input2=str(step_path), verbose=False, metrics='fid,precision,recall')
             # device arg may be 'cuda' or 'cpu'
             if args.device:
                 kwargs['cuda'] = (args.device == 'cuda')
